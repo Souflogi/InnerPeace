646374123f85b49224cbc9122de94cb9435ffa1d
Soufiane <sammahsoufiane01@gmail.com>
2025-11-11 21:51:28 +0100

    Adding Stories section

--- script.js
+++ script.js
@@ -19,3 +19,48 @@
 
 // // start the infinite loop
 // animateRandomItem();
+
+// this is what i added for configuring individual story carousels
+function setupStoryCarousel(carousel) {
+  const slides = Array.from(carousel.querySelectorAll("[data-carousel-slide]"));
+  if (!slides.length) return;
+
+  const prevButton = carousel.querySelector("[data-carousel-prev]");
+  const nextButton = carousel.querySelector("[data-carousel-next]");
+  let activeIndex = 0;
+
+  // this is what i added for changing which slide is visible
+  const showSlide = (index) => {
+    const normalizedIndex = (index + slides.length) % slides.length;
+    slides.forEach((slide, slideIndex) => {
+      if (slideIndex === normalizedIndex) {
+        slide.removeAttribute("hidden");
+        slide.dataset.carouselActive = "true";
+      } else {
+        slide.setAttribute("hidden", "");
+        delete slide.dataset.carouselActive;
+      }
+    });
+    activeIndex = normalizedIndex;
+  };
+
+  showSlide(0);
+
+  // this is what i added for the previous button interaction
+  prevButton?.addEventListener("click", () => {
+    showSlide(activeIndex - 1);
+  });
+
+  // this is what i added for the next button interaction
+  nextButton?.addEventListener("click", () => {
+    showSlide(activeIndex + 1);
+  });
+}
+
+// this is what i added for bootstrapping all of the story carousels
+document.addEventListener("DOMContentLoaded", () => {
+  // this is what i added for finding every carousel instance
+  document
+    .querySelectorAll("[data-carousel]")
+    .forEach((carousel) => setupStoryCarousel(carousel));
+});
